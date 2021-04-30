b70c31d468f3f5c24ad45e29066cc3634a413346
sarthak-g <sarthakgupta041@gmail.com>
2021-04-30 23:39:57 +0530

    crud operations complete

--- Assignment8/index.js
+++ Assignment8/index.js
@@ -3,14 +3,17 @@ const app = express()
 const mongoose = require('mongoose')
 const seedDB = require('./seedDB')
 const path = require('path')
+const methodOverride = require('method-override')
 
 const blogRoutes = require('./routes/blogs');
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(path.join(__dirname, '/public')))
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
 
-mongoose.connect('mongodb://localhost:27017/blogApp', {useNewUrlParser:true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/blogApp', {useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify: false})
 .then(() => {
     console.log("DB CONNECTED!!")
 })
--- Assignment8/routes/blogs.js
+++ Assignment8/routes/blogs.js
@@ -7,4 +7,33 @@ router.get('/', async (req, res) => {
     res.render('home', {blogs})
 })
 
+router.post('/', async (req, res) => {
+    await Blog.create(req.body.blog)
+    res.redirect('/')
+})
+
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
+router.get('/:id', async (req, res) => {
+    const blog = await Blog.findById(req.params.id)
+    res.render('show', {blog})
+})
+
+router.get('/:id/edit', async (req, res) => {
+    const blog = await Blog.findById(req.params.id)
+    res.render('edit', {blog})
+})
+
+router.patch('/:id', async (req, res) => {
+    await Blog.findByIdAndUpdate(req.params.id, req.body.blog)
+    res.redirect(`/${req.params.id}`)
+})
+
+router.delete('/:id/delete', async (req, res) => {
+    await Blog.findByIdAndDelete(req.params.id)
+    res.redirect('/');
+})
+
 module.exports = router;
