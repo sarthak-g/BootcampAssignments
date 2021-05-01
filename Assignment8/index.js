601975c2fcb8c1a4677d7ae141082425b6ab14af
sarthak-g <sarthakgupta041@gmail.com>
2021-05-01 18:48:52 +0530

    success and error flash messages, mongoose error handling

--- Assignment8/index.js
+++ Assignment8/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const seedDB = require('./seedDB')
 const path = require('path')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const blogRoutes = require('./routes/blogs');
 
@@ -20,6 +22,21 @@ mongoose.connect('mongodb://localhost:27017/blogApp', {useNewUrlParser:true, use
 .catch(err => {
     console.log("There is some error with database!!")
     console.log(err)
+})
+
+const sessionConfig = {
+    secret: 'keyboard potato',
+    resave: false,
+    saveUninitialized: true
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next()
 })
 
 // seedDB()
--- Assignment8/routes/blogs.js
+++ Assignment8/routes/blogs.js
@@ -5,12 +5,24 @@ const Blog = require('../models/blogs')
 const Review = require('../models/review')
 
 router.get('/', async (req, res) => {
-    const blogs = await Blog.find({})
-    res.render('home', {blogs})
+    try {
+        const blogs = await Blog.find({})
+        res.render('home', { blogs })
+    }
+    catch(e) {
+        req.flash('error', 'Something went wrong')
+        res.redirect('/')
+    }
 })
 
 router.post('/', async (req, res) => {
-    await Blog.create(req.body.blog)
+    try {
+        await Blog.create(req.body.blog)
+        req.flash('success', 'New blog added')
+    }
+    catch(e) {
+        req.flash('error', 'Something went wrong,Not able to create new blog')
+    }
     res.redirect('/')
 })
 
@@ -19,32 +31,63 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const blog = await Blog.findById(req.params.id).populate('reviews')
-    res.render('show', {blog})
+    try {
+        const blog = await Blog.findById(req.params.id).populate('reviews')
+        res.render('show', { blog })
+    }
+    catch(e) {
+        req.flash('error', 'Not able to find this blog')
+        res.redirect('/')
+    }
 })
 
 router.get('/:id/edit', async (req, res) => {
-    const blog = await Blog.findById(req.params.id)
-    res.render('edit', {blog})
+    try {
+        const blog = await Blog.findById(req.params.id)
+        res.render('edit', { blog })
+    }
+    catch(e) {
+        req.flash('error', 'Not able to find this blog')
+        res.redirect('/')
+    }
 })
 
 router.patch('/:id', async (req, res) => {
-    await Blog.findByIdAndUpdate(req.params.id, req.body.blog)
+    try {
+        await Blog.findByIdAndUpdate(req.params.id, req.body.blog)
+        req.flash('success', 'Blog edited successfully!!')
+    }
+    catch(e) {
+        req.flash('error', 'Not able to update blog')
+    }
     res.redirect(`/${req.params.id}`)
 })
 
 router.delete('/:id/delete', async (req, res) => {
-    await Blog.findByIdAndDelete(req.params.id)
+    try {
+        await Blog.findByIdAndDelete(req.params.id)
+        req.flash('success', 'Blog deleted successfully.')
+    }
+    catch(e) {
+        req.flash('error', 'Something went wrong, not able to delete blog')
+    }
     res.redirect('/');
 })
 
 router.post('/:id/review', async (req, res) => {
-    const blog = await Blog.findById(req.params.id);
-    const review = new Review(req.body.review);
-    blog.reviews.push(review)
-    await review.save()
-    await blog.save()
-    res.redirect(`/${req.params.id}`)
+    try {
+        const blog = await Blog.findById(req.params.id);
+        const review = new Review(req.body.review);
+        blog.reviews.push(review)
+        await review.save()
+        await blog.save()
+        req.flash('success', 'Review successfully added')
+        res.redirect(`/${req.params.id}`)
+    }
+    catch(e) {
+        req.flash('error', 'Something went wrong, not able to add review')
+        res.redirect('/')
+    }
 })
 
 module.exports = router;
